43f14e6d73bd30beaab0f17b73145ee42259f12f
IlyaZar <zarubin@wiso.uni-koeln.de>
2023-03-03 20:09:27 +0100

    More refinements

--- R/mod_07_logistic_Class_data.R
+++ R/mod_07_logistic_Class_data.R
@@ -5,4 +5,8 @@ Data_logistics <- R6::R6Class("Data_logisitics",
                                   for (item in list(...)) {
                                     self$add(item)
                                   }
-                                }))
+                                },
+                                data_logistics_all = NULL,
+                                data_logistics_learn = NULL,
+                                data_logistics_predict1 = NULL,
+                                data_logistics_predict2 = NULL))
