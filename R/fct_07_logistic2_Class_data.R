719e59ee0fe9228bef96d4cf8b08d0cb2c774277
IlyaZar <zarubin@wiso.uni-koeln.de>
2023-03-13 17:28:45 +0100

    Testing some boilerplate

---
+++ R/fct_07_logistic2_Class_data.R
@@ -0,0 +1,7 @@
+#' 07_logistic2_Class_data 
+#'
+#' @description A fct function
+#'
+#' @return The return value, if any, from executing the function.
+#'
+#' @noRd
---
+++ R/utils_07_logistic2_Class_data.R
@@ -0,0 +1,7 @@
+#' 07_logistic2_Class_data 
+#'
+#' @description A utils function
+#'
+#' @return The return value, if any, from executing the utility.
+#'
+#' @noRd
