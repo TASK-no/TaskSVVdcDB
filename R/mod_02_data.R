6d40cc22b8ecf185525c6d6dff29e7bd077a3240
IlyaZar <zarubin@wiso.uni-koeln.de>
2023-03-18 16:14:43 +0100

    Update mod_02_seg data to use R6 class and interact with petite r

--- R/mod_02_data.R
+++ R/mod_02_data.R
@@ -1,31 +1,65 @@
-mod_data_segmentation_srv <- function(id, ...,
-                                      r) {
-  # stopifnot(shiny::is.reactive(sttgs_dc))
-  # stopifnot(shiny::is.reactive(sttgs_q16))
-  # stopifnot(shiny::is.reactive(sttgs_q17))
-  # stopifnot(shiny::is.reactive(sttgs_q14))
-  # stopifnot(shiny::is.reactive(sttgs_q19))
-  ds_raw_all <- list(...)
-  num_ds     <- length(ds_raw_all)
-  year_seq   <- 2020 + 1:num_ds
-  ds_list                 <- vector("list", num_ds)
+mod_data_segmentation_srv <- function(id, r, data_seg) {
   shiny::moduleServer(id, function(input, output, session) {
-    browser()
     shiny::observeEvent(gargoyle::watch("seg_inputs"), {
-      browser()
-      for (i in seq_len(num_ds)) {
-        ds_list[[i]] <- ds_raw_all[[i]] %>%
-          TaskAnalyticsTB::segmentation_analysis(ind1_vals = r[[1]](),
-                                                 settings_q16 = r[[2]](),
-                                                 settings_q17 = r[[3]](),
-                                                 settings_q14 = r[[4]](),
-                                                 settings_q19 = r[[5]]()) %>%
+      data_seg$update_data_segmentation(r)
+      gargoyle::trigger("data_seg")
+    })
+  })
+}
+DataSegmentation <- R6::R6Class(
+  classname = "DataSegmentation",
+  public = list(
+    initialize = function(r, ...) {
+      private$..r <- r
+      private$..data_raw <- list(...)
+      private$..num_ds   <- length(private$..data_raw)
+      private$..year_seq <- 2020 + 1:private$..num_ds
+    },
+    get_data_segmentation = function(id = NULL) {
+      if (is.null(id)) return(private$..data_segmentation)
+      return(self$data_segmentation[[id]])
+    },
+    update_data_segmentation = function(r) {
+      private$..r <- r
+      private$run_segmentation()
+    },
+    update_cat23 = function(list_recodes,
+                            SETTINGS_FACT) {
+      private$run_cat23(list_recodes, SETTINGS_FACT)
+    }
+  ),
+  private = list(
+    ..r = NULL,
+    ..data_raw = NULL,
+    ..num_ds = NULL,
+    ..year_seq = NULL,
+    ..data_segmentation = NULL,
+    run_segmentation = function() {
+      ds_list <- vector("list", private$..num_ds)
+      for (i in seq_len(private$..num_ds)) {
+        ds_list[[i]] <- private$..data_raw[[i]] %>%
+          TaskAnalyticsTB::segmentation_analysis(
+            ind1_vals = private$..r[["seg_inputs"]][["sttgs_DC"]],
+            settings_q16 = private$..r[["seg_inputs"]][["sttgs_Q16"]],
+            settings_q17 = private$..r[["seg_inputs"]][["sttgs_Q17"]],
+            settings_q14 = private$..r[["seg_inputs"]][["sttgs_Q14"]],
+            settings_q19 = private$..r[["seg_inputs"]][["sttgs_Q19"]]) %>%
           dplyr::select(tidyselect::any_of(var_to_use_after_seg),
                         tidyselect::starts_with("kat"))
       }
-      names(ds_list) <- paste0("data_", year_seq)
-      ds_list
-    })
-    return(ds_list)
-  })
-}
+      names(ds_list) <- paste0("data_", private$..year_seq)
+      private$..data_segmentation <- ds_list
+      invisible(ds_list)
+    },
+    run_cat23 = function(list_recodes,
+                         SETTINGS_FACT) {
+      private$..data_segmentation[["data_2023"]] <- private$..data_segmentation[["data_2023"]] %>%
+        TaskAnalyticsTB::recode_qXX_cats(q_names = c("Q36", "Q37",
+                                                     "Q38", "Q40"),
+                                         list_recodes = list_recodes,
+                                         new_names = c("Q36_c", "Q37_c",
+                                                       "Q38_c", "Q40_c"),
+                                         SETTINGS_FACT = SETTINGS_FACT)
+    }
+  )
+)
