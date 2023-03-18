3cd455fda3ae38a66355f7c0c3035715185f0710
IlyaZar <zarubin@wiso.uni-koeln.de>
2023-03-18 16:43:47 +0100

    Update UI and server part to make visuals (tab 1) work completely with R6/r

--- R/app_server.R
+++ R/app_server.R
@@ -1,33 +1,37 @@
-#' The application server-side
+#' the application server-side
 #'
-#' @param input,output,session Internal parameters for {shiny}.
-#'     DO NOT REMOVE.
+#' @param input,output,session internal parameters for {shiny}.
+#'     do not remove.
 #' @import shiny
 #' @noRd
 app_server <- function(input, output, session) {
   shiny::observeEvent(input$my_logout, {
     auth0::logout()
   })
-  r <- shiny::reactiveValues(seg_inputs = shiny::reactiveValues())
+  r <- shiny::reactiveValues(seg_inputs = shiny::reactiveValues(),
+                             datasets = shiny::reactiveValues())
   gargoyle::init("seg_inputs")
+  gargoyle::init("data_seg")
+  gargoyle::init("data_cat")
+
+  data_seg_all <- DataSegmentation$new(r, data_raw_SVV_2021,
+                                       data_raw_SVV_2022,
+                                       data_raw_SVV_2023)
   mod_01_seg_all_server("segmentation", r)
-  browser()
-  data_seg_all <- mod_data_segmentation_srv("segmentation",
-                                            data_raw_SVV_2021,
-                                            data_raw_SVV_2022,
-                                            data_raw_SVV_2023,
-                                            r = r)
+  mod_data_segmentation_srv("segmentation", r = r,
+                            data_seg = data_seg_all)
 
-  data_seg_cat_all <- mod_cat_choices_data_srv("cat_inputs", data_seg_all)
-  data_plot01 <- mod_data_overall_srv("plot_01", data_seg_cat_all)
-  data_plot02 <- mod_data_subplot_srv("plot_02", data_seg_cat_all)
-  data_plot03 <- mod_data_subplot_srv("plot_03", data_seg_cat_all)
+  mod_cat_choices_data_srv("cat_inputs", data_seg_all)
 
-  mod_plot_overall_srv("plot_01", data_plot01)
-  mod_plot_subplot_srv("plot_02", data_plot02, "sub_01")
-  mod_plot_subplot_srv("plot_03", data_plot03, "sub_02")
+  mod_data_overall_srv("plot_01", r, data_seg_all)
+  mod_data_subplot_srv("plot_02", r, data_seg_all, "data_plot02")
+  mod_data_subplot_srv("plot_03", r, data_seg_all, "data_plot03")
+  #
+  mod_plot_overall_srv("plot_01", r)
+  mod_plot_subplot_srv("plot_02", r, "data_plot02", "sub_01")
+  mod_plot_subplot_srv("plot_03", r, "data_plot03", "sub_02")
 
-  data_log01 <- mod_logistic_regression_specs_01_srv("seg_inputs",
-                                                     data_seg_cat_all)
-  mod_logistic_summary_srv("logistic_reg_01", data_log01, "logistic_out_01")
+  # data_log01 <- mod_logistic_regression_specs_01_srv("seg_inputs",
+  #                                                    data_seg_cat_all)
+  # mod_logistic_summary_srv("logistic_reg_01", data_log01, "logistic_out_01")
 }
