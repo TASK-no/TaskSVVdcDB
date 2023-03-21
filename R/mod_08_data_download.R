2940261c7506c6ecfdaef0fc34c535fe97e78e39
IlyaZar <zarubin@wiso.uni-koeln.de>
2023-03-21 15:32:08 +0100

    Refactor module 08 data download
    - encapsulate modal_download into separate function to make module body cleaner
    
    - encapsulate download handler content argument into separate function
    
    - encapsulate download handler filename function into separate function

--- R/mod_08_data_download.R
+++ R/mod_08_data_download.R
@@ -10,41 +10,41 @@
 mod_08_data_download_ui <- function(id){
   ns <- shiny::NS(id)
   shiny::tagList(
-  shiny.semantic::flow_layout(
-    shiny::tagList(
-      add_header(paste("Data-type"), size = 5),
-      shiny.semantic::selectInput(ns("data_type"),
-                                  label = "",
-                                  choices = c("Rådata"),
-                                  multiple = FALSE)
+    shiny.semantic::flow_layout(
+      shiny::tagList(
+        add_header(paste("Data-type"), size = 5),
+        shiny.semantic::selectInput(ns("data_type"),
+                                    label = "",
+                                    choices = c("Rådata"),
+                                    multiple = FALSE)
+      ),
+      shiny::tagList(
+        add_header( paste0("\u00c5", "r "), size = 5),
+        shiny.semantic::selectInput(ns("year_taken"),
+                                    label = "",
+                                    choices = c("2021" = 2021,
+                                                "2022" = 2022,
+                                                "2023" = 2023),
+                                    selected = 2021,
+                                    multiple = TRUE)
+      ),
+      shiny::tagList(
+        add_header("Dataformat", size = 5),
+        shiny.semantic::selectInput(ns("data_format"),
+                                    label = "",
+                                    choices = c("csv (.csv)" = "csv",
+                                                "excel (.xlsx)" = "xls",
+                                                "spss (.sav)" = "spss"),
+                                    multiple = FALSE)
+      )
     ),
-    shiny::tagList(
-      add_header( paste0("\u00c5", "r "), size = 5),
-      shiny.semantic::selectInput(ns("year_taken"),
-                                  label = "",
-                                  choices = c("2021" = 2021,
-                                              "2022" = 2022,
-                                              "2023" = 2023),
-                                  selected = 2021,
-                                  multiple = TRUE)
+    break_vspace(size = "small"),
+    shiny::actionButton(
+      inputId = ns("action"),
+      label = "Nedlasting!",
+      icon = icon("download")
     ),
-    shiny::tagList(
-      add_header("Dataformat", size = 5),
-      shiny.semantic::selectInput(ns("data_format"),
-                                  label = "",
-                                  choices = c("csv (.csv)" = "csv",
-                                              "excel (.xlsx)" = "xls",
-                                              "spss (.sav)" = "spss"),
-                                  multiple = FALSE)
-    )
-  ),
-  break_vspace(size = "small"),
-  shiny::actionButton(
-    inputId = ns("action"),
-    label = "Nedlasting!",
-    icon = icon("download")
-  ),
-  break_vspace(size = "small")
+    break_vspace(size = "small")
   )
 }
 mod_08_data_table_ui <- function(id){
@@ -57,6 +57,11 @@ mod_08_data_table_ui <- function(id){
 mod_08_data_download_server <- function(id, filename) {
   shiny::moduleServer(id, function(input, output, session) {
     ns <- session$ns
+    data_to_download <- shiny::reactive({
+      if (input[["data_type"]] == "Rådata") {
+        get_raw_data(input[["year_taken"]])
+      }
+    })
     shiny::observeEvent(input$action, {
       shiny.semantic::show_modal('download_modal')
     })
@@ -69,56 +74,15 @@ mod_08_data_download_server <- function(id, filename) {
     shiny::observeEvent(
       input$action,
       {
-        shiny.semantic::create_modal(
-          shiny.semantic::modal(
-            shiny.semantic::action_button(ns("cancel"), "Cancel/Close"),
-            id = ns("download_modal"),
-            header = h3("Download the file?", style = "text-align: center;"),
-            footer = tagList(
-              shiny::downloadLink(
-                outputId = ns("dataDownload"),
-                label = "Yes"
-              )
-            ),
-            class = "tiny"
-          ),
-          show = TRUE
-        )
+        modal_download(ns,session_passed = shiny::getDefaultReactiveDomain())
       }
     )
     output$dataDownload <- shiny::downloadHandler(
-      filename = function() {
-        extension <- switch(input$data_format,
-                            "csv" = ".csv",
-                            "xls" = ".xlsx",
-                            "spss" = ".sav")
-        if (input[["data_type"]] == "Rådata") {
-          return(paste0("data_raw_SVV_", input[["year_taken"]], extension))
-        }
-      },
-      content = function(file){
-        if (input$data_format == "csv") {
-          write.csv(data_to_download(), file,
-                    row.names = FALSE, fileEncoding =  "utf-16")
-        } else if(input$data_format == "xls") {
-          xlsx::write.xlsx(data_to_download(), file, row.names = FALSE)
-        } else if(input$data_format == "spss") {
-          haven::write_sav(data_to_download(), file)
-        }
-      }
+      filename = download_handler_filename_01(input = input),
+      content = download_handler_content_01(file, input, data_to_download)
     )
-    data_to_download <- shiny::reactive({
-      if (input[["data_type"]] == "Rådata") {
-        get_raw_data(input[["year_taken"]])
-      }
-    })
     output$table <- reactable::renderReactable({
       reactable::reactable(data_to_download(),
-                             defaultPageSize = 35)})
+                           defaultPageSize = 35)})
   })
 }
-## To be copied in the UI
-# mod_08_data_download_ui("08_data_download_1")
-
-## To be copied in the server
-# mod_08_data_download_server("08_data_download_1")
--- R/mod_08_data_download_fcts.R
+++ R/mod_08_data_download_fcts.R
@@ -5,3 +5,44 @@ get_raw_data <- function(yrs) {
   id_yr <- grepl(as.character(yrs), x = raw_data_names)
   eval(parse(text = raw_data_names[id_yr]))
 }
+modal_download <- function(ns, session_passed) {
+  shiny.semantic::create_modal(
+    shiny.semantic::modal(
+      shiny.semantic::action_button(ns("cancel"), "Cancel/Close"),
+      id = ns("download_modal"),
+      header = htmltools::h3("Download the file?", style = "text-align: center;"),
+      footer = htmltools::tagList(
+        shiny::downloadLink(
+          outputId = ns("dataDownload"),
+          label = "Yes"
+        )
+      ),
+      class = "tiny"
+    ),
+    show = TRUE,
+    session = session_passed
+  )
+}
+download_handler_filename_01 <- function(input) {
+  function() {
+    extension <- switch(input$data_format,
+                        "csv" = ".csv",
+                        "xls" = ".xlsx",
+                        "spss" = ".sav")
+    if (input[["data_type"]] == "Rådata") {
+      return(paste0("data_raw_SVV_", input[["year_taken"]], extension))
+    }
+  }
+}
+download_handler_content_01 <- function(file, input, data_set){
+  function(file) {
+    if (input$data_format == "csv") {
+      write.csv(data_set(), file,
+                row.names = FALSE, fileEncoding =  "utf-16")
+    } else if(input$data_format == "xls") {
+      xlsx::write.xlsx(data_set(), file, row.names = FALSE)
+    } else if(input$data_format == "spss") {
+      haven::write_sav(data_set(), file)
+    }
+  }
+}
