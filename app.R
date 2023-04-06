9e27c076e2fbf92da9f9caff422ccb73ebee907e
IlyaZar <zarubin@wiso.uni-koeln.de>
2023-04-06 10:25:23 +0200

    refactor: add prelims for deployment via docker
    - app.R should work without auth0 for the moment
    - dev_run.R only minor changes coming from the use of vscode

--- app.R
+++ app.R
@@ -3,14 +3,18 @@
 # Or use the blue button on top of this file
 # options(repos = c(getOption("repos"),
 #         "https://github.com/Zeta-Display/ZetaAnalyticsTB"))
+# library(magrittr)
+# library(TaskAnalyticsTB)
+# library(shiny)
+# library(shiny.semantic)
 # library(auth0)
 pkgload::load_all(export_all = FALSE,
                   helpers = FALSE,
                   attach_testthat = FALSE)
 options("golem.app.prod" = TRUE,
         shiny.port = 8080,
-        auth0_disable = FALSE,
+        auth0_disable = TRUE,
         auth0_config_file = system.file("app/_auth0.yml",
                                         package = "TaskSVVdcDB"))
-TaskSVVdcDB::run_app_auth0()
-# TaskSVVdcDB::run_app() # add parameters here (if any)
+# TaskSVVdcDB::run_app_auth0()
+TaskSVVdcDB::run_app() # add parameters here (if any)
