4b8a0fdc2d2a0640b9201aefab3f337a70b78add
IlyaZar <zarubin@wiso.uni-koeln.de>
2023-04-23 14:41:17 +0200

    update internal logistics data handling
    - set update to logistics data "all", "train" and "prdct" to private
    - adjust getters accordingly

--- R/mod_06_logistic_data_class.R
+++ R/mod_06_logistic_data_class.R
@@ -8,12 +8,12 @@ DataLogistics <- R6::R6Class(
       }
     },
     get_data_logistics = function(type = "all") {
+      browser()
       if(type == "all") return(private$..data_logistics)
       if(type == "train") {
-        return(private$..data_logistics[seq_len(private$..num_obs_train), ])
+        return(private$..data_logistics_train)
       } else if (type == "prdct") {
-        id_obs_prdct <- (private$..num_obs_train + 1):private$..num_obs_all
-        return(private$..data_logistics[id_obs_prdct, ])
+        return(private$..data_logistics_prdct)
       }
     },
     get_model_logistics = function() {
@@ -36,22 +36,11 @@ DataLogistics <- R6::R6Class(
     },
     update_data_base = function(data_seg) {
       private$..data_logistics_base <- data_seg
-      self$update_data_logistics()
+      private$update_data_logistics()
     },
     update_yrs = function(years) {
       private$..year_taken <- years
-      self$update_data_logistics()
-
-    },
-    update_data_logistics = function() {
-      private$..data_logistics <- private$..data_logistics_base %>%
-        get_data_logistics_all(private$..year_taken)
-      if (!is.null(private$..data_logistics)) {
-        private$..num_obs_all   <- nrow(private$..data_logistics)
-      }
-      private$..num_obs_train <- min(private$..num_obs_all,
-                                     private$..num_obs_train)
-      private$..num_obs_prdct <- private$..num_obs_all - private$..num_obs_train
+      private$update_data_logistics()
     },
     update_mod = function(dep, reg, exp) {
       private$..mod <- deparse_input_logistic_to_model(
@@ -65,9 +54,30 @@ DataLogistics <- R6::R6Class(
     ..year_taken = NULL,
     ..data_logistics_base = NULL,
     ..data_logistics = NULL,
+    ..data_logistics_train = NULL,
+    ..data_logistics_prdct = NULL,
     ..mod = NULL,
     ..num_obs_all = 1925,
     ..num_obs_train = 1000,
-    ..num_obs_prdct = 0
+    ..num_obs_prdct = 925,
+    update_data_logistics = function() {
+      private$..data_logistics <- private$..data_logistics_base %>%
+        get_data_logistics_all(private$..year_taken)
+      if (!is.null(private$..data_logistics)) {
+        private$..num_obs_all   <- nrow(private$..data_logistics)
+      }
+      private$..num_obs_train <- min(private$..num_obs_all,
+                                     private$..num_obs_train)
+      private$..num_obs_prdct <- private$..num_obs_all - private$..num_obs_train
+      private$update_data_log_train()
+      private$update_data_log_prdct()
+    },
+    update_data_log_train = function() {
+      private$..data_logistics_train <- private$..data_logistics[seq_len(private$..num_obs_train), ]
+    },
+    update_data_log_prdct = function() {
+      id_obs_prdct <- (private$..num_obs_train + 1):private$..num_obs_all
+      private$..data_logistics_prdct <- private$..data_logistics[id_obs_prdct, ]
+    }
   )
 )
