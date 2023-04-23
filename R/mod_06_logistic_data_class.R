66f8c3eeeacd169e50edf22265760d4e38dee665
IlyaZar <zarubin@wiso.uni-koeln.de>
2023-04-23 09:53:15 +0200

    refactor logisitic class for binary classification
    - add num_obs_train as a class parameter
    - add num_obs_all, num_obs_prdct as internal parameters
    - add getters for training and prediction data sets

--- R/mod_06_logistic_data_class.R
+++ R/mod_06_logistic_data_class.R
@@ -1,19 +1,37 @@
 DataLogistics <- R6::R6Class(
   "DataLogistics",
   public = list(
-    initialize = function(data_seg) {
+    initialize = function(data_seg, num_obs_train) {
       private$..data_logistics_base <- data_seg
+      private$..num_obs_train <- num_obs_train
     },
-    get_data_logistics = function() {
-      private$..data_logistics
+    get_data_logistics = function(type = "all") {
+      if(type = "all") return(private$..data_logistics)
+      if(type = "train") {
+        return(private$..data_logistics[seq_len(private$..num_obs_train), ])
+      } else if (type = "prdct") {
+        id_obs_prdct <- (private$..num_obs_train + 1):private$..num_obs_all
+        return(private$..data_logistics[id_obs_prdct, ])
+      }
     },
     get_model_logistics = function() {
       private$..mod
+    },
+    get_num_obs = function(type) {
+      if(type == "all") return(nrow(private$..data_logistics))
+      if(type == "train") return(private$..num_obs_train)
+      if(type == "prdct") return(private$..num_obs_prdct)
+    },
+    update_num_obs_train = function(num_obs_train) {
+      private$..num_obs_train <- num_obs_train
+      private$..num_obs_prdct <- private$..num_obs_all - private$..num_obs_train
     },
     update_data_base = function(data_seg) {
       private$..data_logistics_base <- data_seg
       private$..data_logistics <- private$..data_logistics_base %>%
         get_data_logistics_all(private$..year_taken)
+      private$..num_obs_all   <- nrow(private$..data_logistics)
+      private$..num_obs_prdct <- private$..num_obs_all - private$..num_obs_train
     },
     update_yrs = function(years) {
       private$..year_taken <- years
@@ -32,6 +50,9 @@ DataLogistics <- R6::R6Class(
     ..year_taken = NULL,
     ..data_logistics_base = NULL,
     ..data_logistics = NULL,
-    ..mod = NULL
+    ..mod = NULL,
+    ..num_obs_all,
+    ..num_obs_train,
+    ..num_obs_prdct
   )
 )
