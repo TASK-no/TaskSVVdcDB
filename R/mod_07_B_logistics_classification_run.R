acb9c2c6ec9b97d19ea9ea3309066264c08c6890
IlyaZar <zarubin@wiso.uni-koeln.de>
2023-04-24 14:07:52 +0200

    refactor roc curve to work correctly

--- R/mod_07_B_logistics_classification_run.R
+++ R/mod_07_B_logistics_classification_run.R
@@ -14,7 +14,11 @@ modlogistics_classification_run_ui <- function(id){
                            label = "Klassifisering av kjørelogistikk!",
                            shiny.semantic::icon("horizontally flipped cloud")
     ),
-    shiny::plotOutput(ns("plotROC"))
+    shiny.semantic::flow_layout(
+      shiny::htmlOutput(ns("plotROC")),
+      shiny::htmlOutput(ns("roc_text")),
+      min_cell_width = "375px",
+      column_gap = "50px")
   )
 }
 
@@ -43,20 +47,37 @@ mod_logistics_classification_run_server <- function(id, class_logistics_data){
         data_prediction,
         type = "response"
       )
-
       classification_infos <- TaskAnalyticsTB:::get_cls_infos(
         true_ONES,
         predictions
       )
       if (!(is.null(true_ONES) || is.null(predictions))) {
-        output$plotROC <- shiny::renderPlot({
-          InformationValue::plotROC(
-          true_ONES,
-          predictions,
-          Show.labels = TRUE)
+        df_tmp <- data.frame(true_ones = true_ONES, preds = predictions)
+        roc_plot <- ggplot2::ggplot(
+          df_tmp,
+          ggplot2::aes(d = .data$true_ones, m = .data$preds)) +
+          plotROC::geom_roc(labelround = 2, n.cuts = 15)
+        roc_plot <- roc_plot +
+          plotROC::style_roc(theme = ggplot2::theme_grey) +
+          ggplot2::ggtitle("ROC curve") +
+          ggplot2::annotate("text", x = .75, y = .25,
+                            label = paste(
+                              "AUC =",
+                              round(plotROC::calc_auc(roc_plot)$AUC, 4))) +
+          ggplot2::scale_x_continuous("1 - Specificity (FPR)") +
+          ggplot2::scale_y_continuous("Sensitivity (TPR)")
+        output$plotROC <- shiny::renderUI({
+          htmltools::HTML(
+            plotROC::export_interactive_roc(roc_plot)
+            )})
+        output$roc_text <- shiny::renderUI({
+          htmltools::HTML(get_roc_text())
           })
       } else {
-        output$plotROC <- NULL
+        output$plotROC  <- NULL
+        output$roc_text <- shiny::renderUI({
+          htmltools::HTML("")
+        })
       }
       }
     )
