20cb6dad879d63df01c7530696abbc666e82de83
IlyaZar <zarubin@wiso.uni-koeln.de>
2024-02-08 18:16:33 +0100

    refactoring: make use of previous helper functions

--- data-raw/raw_data_cleaning.R
+++ data-raw/raw_data_cleaning.R
@@ -21,41 +21,35 @@ data_raw_SVV_2024 <- read_and_distinct(
 
 data_raw_SVV_2021 <- data_raw_SVV_2021 %>%
   recode_utdanning(levels_range = 1:4) %>%
-  recode_leder_c(leder_var = "leder_c", method = "replace")
+  recode_leder_c(leder_var = "leder_c", method = "replace") %>%
+  recode_questions(
+    questions_prefix = "Q25r",
+    seq_questions = 1:6
+  )
 
 data_raw_SVV_2022 <- data_raw_SVV_2022 %>%
   recode_utdanning(utdanning_name = "Utdanning", levels_range = 1:5) %>%
-  recode_leder_c(leder_var = "Q32", method = "direct")
+  recode_leder_c(leder_var = "Q32", method = "direct") %>%
+  recode_questions(
+    questions_prefix = "Q25r",
+    seq_questions = 1:7
+  )
 
 data_raw_SVV_2023 <- data_raw_SVV_2023 %>%
   recode_utdanning(utdanning_name = "Utdanningsnivå", levels_range = 1:5) %>%
-  recode_leder_c(leder_var = "Q32", method = "direct")
+  recode_leder_c(leder_var = "Q32", method = "direct") %>%
+  recode_questions(
+    questions_prefix = "Q25r",
+    seq_questions = 1:7
+  )
 
-questions_relevel <- paste0("Q25r", 1:7)
-for (i in questions_relevel) {
-  data_raw_SVV_2023[[i]] <- factor(data_raw_SVV_2023[[i]])
-  levels(data_raw_SVV_2023[[i]]) <- c(levels(data_raw_SVV_2023[[i]]), 1:8)
-  levels(data_raw_SVV_2023[[i]])[c(1, 2, 8)] <- "Svært uenig"
-  levels(data_raw_SVV_2023[[i]])[c(2, 3, 4)] <- "enig"
-  levels(data_raw_SVV_2023[[i]])[c(3, 4)] <- "Svært enig"
-}
-
-questions_relevel <- paste0("Q25r", 1:7)
-for (i in questions_relevel) {
-  data_raw_SVV_2022[[i]] <- factor(data_raw_SVV_2022[[i]])
-  levels(data_raw_SVV_2022[[i]]) <- c(levels(data_raw_SVV_2022[[i]]), 1:8)
-  levels(data_raw_SVV_2022[[i]])[c(1, 2, 8)] <- "Svært uenig"
-  levels(data_raw_SVV_2022[[i]])[c(2, 3, 4)] <- "enig"
-  levels(data_raw_SVV_2022[[i]])[c(3, 4)] <- "Svært enig"
-}
-questions_relevel <- paste0("Q25r", 1:6)
-for (i in questions_relevel) {
-  data_raw_SVV_2021[[i]] <- factor(data_raw_SVV_2021[[i]])
-  levels(data_raw_SVV_2021[[i]]) <- c(levels(data_raw_SVV_2021[[i]]), 1:8)
-  levels(data_raw_SVV_2021[[i]])[c(1, 2, 8)] <- "Svært uenig"
-  levels(data_raw_SVV_2021[[i]])[c(2, 3, 4)] <- "enig"
-  levels(data_raw_SVV_2021[[i]])[c(3, 4)] <- "Svært enig"
-}
+data_raw_SVV_2024 <- data_raw_SVV_2024 %>%
+  recode_utdanning(utdanning_name = "Utdanning", levels_range = 1:5) %>%
+  recode_leder_c(leder_var = "Q32", method = "direct") %>%
+  recode_questions(
+    questions_prefix = "Q25r",
+    seq_questions = c(1:5, 7)
+  )
 
 data_raw_SVV_2023$Q22 <- factor(data_raw_SVV_2023$Q22,
                                 labels = names(attr(
