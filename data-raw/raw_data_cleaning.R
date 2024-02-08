8c7b2d7f23201e8ef47dc420abac869eda6e2c0b
IlyaZar <zarubin@wiso.uni-koeln.de>
2024-02-08 18:31:34 +0100

    refactor: make adjustments to previous code and add useful comments

--- data-raw/raw_data_cleaning.R
+++ data-raw/raw_data_cleaning.R
@@ -2,77 +2,58 @@ library(magrittr)
 library(TaskAnalyticsTB)
 pth_to_data <- "./data-raw"
 
+# Read and clean data for each year, removing duplicate rows
+# 2021 dataset
 data_raw_SVV_2021 <- read_and_distinct(
   file.path(pth_to_data, "2021"),
   "SNA80543_210212New_BG_weighted_Tables_JUS_ANT_Ilya.sav"
 )
+# 2022 dataset
 data_raw_SVV_2022 <- read_and_distinct(
   file.path(pth_to_data, "2022"),
   "SNA93333_220131_enriched_weighted.sav"
 )
+# 2023 dataset
 data_raw_SVV_2023 <- read_and_distinct(
   file.path(pth_to_data, "2023"),
   "SNA107320_230214_weighted.sav"
 )
+# 2024 dataset
 data_raw_SVV_2024 <- read_and_distinct(
   file.path(pth_to_data, "2024"),
   "SNA127220_240207_weighted.sav"
 )
 
+# Apply a series of cleaning and recoding functions to each dataset
+# Recoding utdanning, leder_c, specific questions, and Q22 for 2021 dataset
 data_raw_SVV_2021 <- data_raw_SVV_2021 %>%
   recode_utdanning(levels_range = 1:4) %>%
   recode_leder_c(leder_var = "leder_c", method = "replace") %>%
-  recode_questions(
-    questions_prefix = "Q25r",
-    seq_questions = 1:6
-  )
+  recode_questions(questions_prefix = "Q25r", seq_questions = 1:6) %>%
+  recode_Q22()
 
+# Similar recoding steps for 2022 dataset
 data_raw_SVV_2022 <- data_raw_SVV_2022 %>%
   recode_utdanning(utdanning_name = "Utdanning", levels_range = 1:5) %>%
   recode_leder_c(leder_var = "Q32", method = "direct") %>%
-  recode_questions(
-    questions_prefix = "Q25r",
-    seq_questions = 1:7
-  )
+  recode_questions(questions_prefix = "Q25r", seq_questions = 1:7) %>%
+  recode_Q22()
 
+# Similar recoding steps for 2023 dataset
 data_raw_SVV_2023 <- data_raw_SVV_2023 %>%
   recode_utdanning(utdanning_name = "Utdanningsnivå", levels_range = 1:5) %>%
   recode_leder_c(leder_var = "Q32", method = "direct") %>%
-  recode_questions(
-    questions_prefix = "Q25r",
-    seq_questions = 1:7
-  )
+  recode_questions(questions_prefix = "Q25r", seq_questions = 1:7) %>%
+  recode_Q22()
 
+# Similar recoding steps for 2024 dataset, with a slight variation in questions
 data_raw_SVV_2024 <- data_raw_SVV_2024 %>%
   recode_utdanning(utdanning_name = "Utdanning", levels_range = 1:5) %>%
   recode_leder_c(leder_var = "Q32", method = "direct") %>%
-  recode_questions(
-    questions_prefix = "Q25r",
-    seq_questions = c(1:5, 7)
-  )
+  recode_questions(questions_prefix = "Q25r", seq_questions = c(1:5, 7)) %>%
+  recode_Q22()
 
-data_raw_SVV_2023$Q22 <- factor(data_raw_SVV_2023$Q22,
-                                labels = names(attr(
-                                  data_raw_SVV_2023$Q22,
-                                  "labels"
-                                )[1:5])
-)
-levels(data_raw_SVV_2023$Q22)[c(1, 5)] <- "Ikke i det hele tatt"
-data_raw_SVV_2022$Q22 <- factor(data_raw_SVV_2022$Q22,
-                                labels = names(attr(
-                                  data_raw_SVV_2022$Q22,
-                                  "labels"
-                                )[1:5])
-)
-levels(data_raw_SVV_2022$Q22)[c(1, 5)] <- "Ikke i det hele tatt"
-data_raw_SVV_2021$Q22 <- factor(data_raw_SVV_2021$Q22,
-                                labels = names(attr(
-                                  data_raw_SVV_2021$Q22,
-                                  "labels"
-                                )[1:5])
-)
-levels(data_raw_SVV_2021$Q22)[c(1, 5)] <- "Ikke i det hele tatt"
-
+# Additional specific recoding for the 2023 dataset using a custom function
 data_raw_SVV_2023 <- data_raw_SVV_2023 %>%
   TaskAnalyticsTB::recode_qXX_rVals(
     q_names = c("Q38", "Q39", "Q40"),
@@ -86,19 +67,39 @@ data_raw_SVV_2023 <- data_raw_SVV_2023 %>%
     )
   )
 
+# Select relevant variables for saving and verify integrity against saved
+# versions Verification for 2021 dataset
+data_raw_SVV_2021_to_save <- data_raw_SVV_2021 %>%
+  dplyr::select(
+    tidyselect::any_of(get_var_to_use_after_seg()),
+    tidyselect::starts_with(get_var_to_use(2))
+  )
+load("./data/data_raw_SVV_2021.rda")
+stopifnot(identical(data_raw_SVV_2021, data_raw_SVV_2021_to_save))
 
-data_raw_SVV_2021 <- data_raw_SVV_2021 %>%
+# Verification for 2022 dataset
+data_raw_SVV_2022_to_save <- data_raw_SVV_2022 %>%
   dplyr::select(
-    tidyselect::any_of(var_to_use_after_seg),
-    tidyselect::starts_with(var_to_use_before_seg)
+    tidyselect::any_of(get_var_to_use_after_seg()),
+    tidyselect::starts_with(get_var_to_use(2))
   )
-data_raw_SVV_2022 <- data_raw_SVV_2022 %>%
+load("./data/data_raw_SVV_2022.rda")
+stopifnot(identical(data_raw_SVV_2022, data_raw_SVV_2022_to_save))
+
+# Verification for 2023 dataset
+data_raw_SVV_2023_to_save <- data_raw_SVV_2023 %>%
   dplyr::select(
-    tidyselect::any_of(var_to_use_after_seg),
-    tidyselect::starts_with(var_to_use_before_seg)
+    tidyselect::any_of(get_var_to_use_after_seg()),
+    tidyselect::starts_with(get_var_to_use(2))
   )
-data_raw_SVV_2023 <- data_raw_SVV_2023 %>%
+load("./data/data_raw_SVV_2023.rda")
+stopifnot(identical(data_raw_SVV_2023, data_raw_SVV_2023_to_save))
+
+# Verification for 2024 dataset
+data_raw_SVV_2024_to_save <- data_raw_SVV_2024 %>%
   dplyr::select(
-    tidyselect::any_of(var_to_use_after_seg),
-    tidyselect::starts_with(var_to_use_before_seg)
+    tidyselect::any_of(get_var_to_use_after_seg()),
+    tidyselect::starts_with(get_var_to_use(2))
   )
+load("./data/data_raw_SVV_2024.rda")
+stopifnot(identical(data_raw_SVV_2024, data_raw_SVV_2024_to_save))
