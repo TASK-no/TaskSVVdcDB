9e455f8897ddaab42a5431fcf1a91ce59349cfbd
IlyaZar <zarubin@wiso.uni-koeln.de>
2024-02-10 18:43:36 +0100

    feat: add utils support for 2024 data inside app

--- R/mod_utils.R
+++ R/mod_utils.R
@@ -150,22 +150,23 @@ filter_for_samansi_leder_cat <- function(ds, year, SAMANSI, LEDER,
                                          CAT_Q36, CAT_Q37,
                                          CAT_Q38, CAT_Q40) {
   data_out <- ds
+  year_to_segment_style <- c(2023, 2024)
   if (SAMANSI && year == 2022) {
     data_out <- data_out %>% dplyr::filter(.data$SamAnsi == 1)
   }
   if (LEDER) {
     data_out <- data_out %>% dplyr::filter(.data$leder_c == "Ja")
   }
-  if (CAT_Q36 && year == 2023) {
+  if (CAT_Q36 && year %in% year_to_segment_style) {
     data_out <- data_out %>% dplyr::filter(.data$Q36_c == "Ja")
   }
-  if (CAT_Q37 && year == 2023) {
+  if (CAT_Q37 && year %in% year_to_segment_style) {
     data_out <- data_out %>% dplyr::filter(.data$Q37_c == "Ja")
   }
-  if (CAT_Q38 && year == 2023) {
+  if (CAT_Q38 && year %in% year_to_segment_style) {
     data_out <- data_out %>% dplyr::filter(.data$Q38_c == "Ja")
   }
-  if (CAT_Q40 && year == 2023) {
+  if (CAT_Q40 && year %in% year_to_segment_style) {
     data_out <- data_out %>% dplyr::filter(.data$Q40_c == "Ja")
   }
   return(data_out)
