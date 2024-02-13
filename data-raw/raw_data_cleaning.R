0ae35441ed6c8c183433e5cc6db961f2543f660c
IlyaZar <zarubin@wiso.uni-koeln.de>
2024-02-13 20:31:23 +0100

    feat: raw data cleaning script gets updates

--- data-raw/raw_data_cleaning.R
+++ data-raw/raw_data_cleaning.R
@@ -88,7 +88,13 @@ data_raw_SVV_2021_to_save <- data_raw_SVV_2021 %>%
   dplyr::select(
     tidyselect::any_of(get_var_to_use_after_seg()),
     tidyselect::starts_with(get_var_to_use(2))
-  )
+  ) %>%
+  fix_type_to_factor(list_vars = list_vars_factors_info)
+
+
+update_data_set(data_raw_SVV_2021_to_save,
+                "data_raw_SVV_2021",
+                "./data/data_raw_SVV_2021.rda")
 load("./data/data_raw_SVV_2021.rda")
 stopifnot(identical(data_raw_SVV_2021, data_raw_SVV_2021_to_save))
 
@@ -97,7 +103,11 @@ data_raw_SVV_2022_to_save <- data_raw_SVV_2022 %>%
   dplyr::select(
     tidyselect::any_of(get_var_to_use_after_seg()),
     tidyselect::starts_with(get_var_to_use(2))
-  )
+  ) %>%
+  fix_type_to_factor(list_vars = list_vars_factors_info)
+update_data_set(data_raw_SVV_2022_to_save,
+                "data_raw_SVV_2022",
+                "./data/data_raw_SVV_2022.rda")
 load("./data/data_raw_SVV_2022.rda")
 stopifnot(identical(data_raw_SVV_2022, data_raw_SVV_2022_to_save))
 
@@ -106,7 +116,11 @@ data_raw_SVV_2023_to_save <- data_raw_SVV_2023 %>%
   dplyr::select(
     tidyselect::any_of(get_var_to_use_after_seg()),
     tidyselect::starts_with(get_var_to_use(2))
-  )
+  ) %>%
+  fix_type_to_factor(list_vars = list_vars_factors_info)
+update_data_set(data_raw_SVV_2023_to_save,
+                "data_raw_SVV_2023",
+                "./data/data_raw_SVV_2023.rda")
 load("./data/data_raw_SVV_2023.rda")
 stopifnot(identical(data_raw_SVV_2023, data_raw_SVV_2023_to_save))
 
@@ -116,6 +130,26 @@ data_raw_SVV_2024_to_save <- data_raw_SVV_2024 %>%
     tidyselect::any_of(get_var_to_use_after_seg()),
     tidyselect::starts_with(get_var_to_use(2)),
     tidyselect::starts_with(get_var_to_use(6))
-  )
+  ) %>%
+  fix_type_to_factor(list_vars = list_vars_factors_info)
+update_data_set(data_raw_SVV_2024_to_save,
+                "data_raw_SVV_2024",
+                "./data/data_raw_SVV_2024.rda")
 load("./data/data_raw_SVV_2024.rda")
 stopifnot(identical(data_raw_SVV_2024, data_raw_SVV_2024_to_save))
+
+# load("/home/iz/Downloads/data_logistics_all.rda")
+# test_data <- data_logistics_all
+
+# data_short2 <- data_short
+# data_short2$fullfortkurs <- factor(data_short2$fullfortkurs, ordered = TRUE)
+# data_short2$kat_kommunikasjon <- as.numeric(data_short2$kat_kommunikasjon)
+# data_short2$kat_kommunikasjon <- data_short2$kat_kommunikasjon - 1
+# summary(stats::glm(model_formula, data = data_short, family = stats::binomial(link = "logit")))
+# summary(stats::glm(model_formula, data = data_short2, family = stats::binomial(link = "logit")))
+#
+#
+# testme_fullfort  <- data_short %>% dplyr::filter(.data$fullfortkurs == "fullfort")
+# testme_paebegynt <- data_short %>% dplyr::filter(.data$fullfortkurs == "paebegynt")
+#
+# table(testme_fullfort$kat_kommunikasjon)
