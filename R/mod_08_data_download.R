fd5b0f9b93704a2b2366a4b96f5bf22a28773a41
IlyaZar <zarubin@wiso.uni-koeln.de>
2024-02-19 13:54:14 +0100

    fix: update UI to generate proper reactable table view without haven_labelled cols
    - which failed with C memory dump before

--- R/mod_08_data_download.R
+++ R/mod_08_data_download.R
@@ -33,18 +33,31 @@ mod_08_data_download_ui <- function(id) {
         )
       )
     ),
-    break_vspace(size = "small"),
-    shiny::actionButton(
-      inputId = ns("action"),
-      label = "Nedlasting!",
-      icon = shiny.semantic::icon("download")
+    shiny.semantic::flow_layout(
+      shiny::tagList(
+        break_vspace(size = "small"),
+        shiny::actionButton(
+          inputId = ns("action_data_update"),
+          label = "Oppdater datasett!",
+          icon = shiny.semantic::icon("home")
+        )
+      ),
+      shiny::tagList(
+        break_vspace(size = "small"),
+        shiny::actionButton(
+          inputId = ns("action_data_download"),
+          label = "Nedlasting!",
+          icon = shiny.semantic::icon("download")
+        )
+      )
     ),
     break_vspace(size = "small")
   )
 }
 mod_08_data_table_ui <- function(id) {
   ns <- shiny::NS(id)
-  reactable::reactableOutput(ns("table"))
+  reactable::reactableOutput(ns("table_data_preview"))
+  # shiny::tableOutput(ns("table_data_preview"))
 }
 #' 08_data_download Server Functions
 #'
@@ -52,28 +65,40 @@ mod_08_data_table_ui <- function(id) {
 mod_08_data_download_server <- function(id, r, data_seg, data_log) {
   shiny::moduleServer(id, function(input, output, session) {
     ns <- session$ns
-    data_to_download <- shiny::reactive({
+    data_to_download <- shiny::reactive(
+      {
       get_data(input, r, data_seg, data_log)
-    })
-    shiny::observeEvent(input$action, {
-      shiny.semantic::show_modal("download_modal")
-    })
-    shiny::observeEvent(input$cancel, {
-      shiny.semantic::hide_modal("download_modal")
-    })
+      }
+    ) %>% shiny::bindEvent(gargoyle::watch("action_data_regenerate"))
     shiny::observeEvent(
-      input$action,
+      input$action_data_download_cancel,
+      {
+      shiny.semantic::hide_modal("download_modal")
+      }
+    )
+    shiny::observeEvent(
+      input$action_data_download,
       {
         modal_download(ns, session_passed = shiny::getDefaultReactiveDomain())
+        gargoyle::trigger("action_data_regenerate")
+      }
+    )
+    shiny::observeEvent(
+      input$action_data_update,
+      {
+        gargoyle::trigger("action_data_regenerate")
       }
     )
     output$dataDownload <- shiny::downloadHandler(
       filename = download_handler_filename_01(input = input),
       content = download_handler_content_01(file, input, data_to_download)
     )
-    output$table <- reactable::renderReactable({
-      reactable::reactable(data_to_download(),
-        defaultPageSize = 35
+    output$table_data_preview <- reactable::renderReactable({
+      tmp_data <- data_to_download()
+      tmp_data <- get_data_no_haven_labelled(tmp_data)
+      reactable::reactable(
+        tmp_data,
+        defaultPageSize = 10
       )
     })
   })
