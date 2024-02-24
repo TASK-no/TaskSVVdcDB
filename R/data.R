b20760a2d9c86d554e61d2e7ec85cbeacb92d933
IlyaZar <zarubin@wiso.uni-koeln.de>
2024-02-24 16:41:29 +0100

    docs: add script for documentation of data sets

---
+++ R/data.R
@@ -0,0 +1,51 @@
+#' Data of SVV from Norstat
+#'
+#' Information on employees and their questionaire responses for 2021.
+#'
+#' @format ## `data_raw_SVV_2021`
+#' A data frame with 1925 rows and 53 columns:
+#' \describe{
+#'   \item{SamGender}{the employeer gender}
+#'   \item{....}{...}
+#' }
+#' @source as provided from Norstat in 2021
+"data_raw_SVV_2021"
+
+#' Data of SVV from Norstat
+#'
+#' Information on employees and their questionaire responses for 2022.
+#'
+#' @format ## `data_raw_SVV_2022`
+#' A data frame with 1941 rows and 55 columns:
+#' \describe{
+#'   \item{SamGender}{the employeer gender}
+#'   \item{....}{...}
+#' }
+#' @source as provided from Norstat in 2022
+"data_raw_SVV_2022"
+
+#' Data of SVV from Norstat
+#'
+#' Information on employees and their questionaire responses for 2023.
+#'
+#' @format ## `data_raw_SVV_2023`
+#' A data frame with 2090 rows and 63 columns:
+#' \describe{
+#'   \item{SamGender}{the employeer gender}
+#'   \item{....}{...}
+#' }
+#' @source as provided from Norstat in 2023
+"data_raw_SVV_2023"
+
+#' Data of SVV from Norstat
+#'
+#' Information on employees and their questionaire responses for 2024.
+#'
+#' @format ## `data_raw_SVV_2024`
+#' A data frame with 1906 rows and 71 columns:
+#' \describe{
+#'   \item{SamGender}{the employeer gender}
+#'   \item{....}{...}
+#' }
+#' @source as provided from Norstat in 2024
+"data_raw_SVV_2024"
